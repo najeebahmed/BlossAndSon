5fc10954f0b41cf6121674848af277a949f4a53b
Grant <grantddickerson@yahoo.com>
2021-03-06 00:03:44 -0700

    GD +1

--- src/queries/base.py
+++ src/queries/base.py
@@ -2,6 +2,7 @@ import requests
 import getauth
 import http.client
 import json
+import math
 
 conn = http.client.HTTPSConnection("api.mindbodyonline.com")
 
@@ -11,23 +12,25 @@ HEADERS = {
     'Authorization': getauth.GetAccessToken()
     }
 
-def GetRequest(uri, array_name):
+def GetRequest(uri, array_name, onepageonly=False):
     """The point of this function is..."""
-    pagesize = 1
+    pages_remaining = 9999
     results = []
     offset = 0
-    while pagesize > 0:
+    while pages_remaining > 0:
         conn.request("GET", f'{uri}?Limit=200&offset={offset}', headers=HEADERS)
-        offset = offset + 200
+        offset += 200
         res = conn.getresponse()
         data = json.loads(res.read().decode('utf-8'))
 
-    if "PaginationResponse" in data:
-        pagesize = data["PaginationResponse"]["PageSize"]
-    else:
-        print(data)
-        pagesize = 0
-    if (array_name in data):
-        results.extend(data[array_name])
-
+        if "PaginationResponse" in data:
+            if pages_remaining == 9999:
+                pages_remaining = math.ceil(data["PaginationResponse"]["TotalResults"] / 200)
+            if array_name in data:
+                results.extend(data[array_name])
+            pages_remaining -= 1
+        else:
+            print(data)
+            pages_remaining = 0
+        
     return results
