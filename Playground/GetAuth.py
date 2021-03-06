74980a35b679cd59a11c0e5a056c8928bd51c460
Grant <grantddickerson@yahoo.com>
2021-03-05 21:15:30 -0700

    Added GetAuth File

---
+++ Playground/GetAuth.py
@@ -0,0 +1,19 @@
+import http.client
+import json
+
+conn = http.client.HTTPSConnection("api.mindbodyonline.com")
+
+headers = {
+    'Content-Type': "application/json",
+    'Api-Key': "a0e9d29c240c45af8873fbd407844e4c",
+    'SiteId': "-99",
+    'User-Agent': "DBA"
+    }
+
+#TO GET AN AUTHORIZATION NUMBER (like: 3f8eb187d220492fbaf456a51c6962bee3a62e86070b4a5194344f88ee06fa3d)
+payload = "{\r\n\t\"Username\": \"Siteowner\",\r\n\t\"Password\": \"apitest1234\"\r\n}"
+conn.request("POST", "/public/v6/usertoken/issue", payload, headers)
+res = conn.getresponse()
+data = res.read()
+jsondata = json.loads(data.decode("utf-8"))
+print(jsondata["AccessToken"])
