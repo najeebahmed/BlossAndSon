ffd6a6d9d285ab09ed3ea0c3c8d2b08efa6c5556
Najeeb Ahmed <najeebahmed87@yahoo.com>
2021-03-26 22:04:24 -0700

    Update client visits

--- src/queries/clientvisits.py
+++ src/queries/clientvisits.py
@@ -1,3 +1,15 @@
 from base import GetRequest
+import datetime
 
-clients = GetRequest("/public/v6/client/clients", "Clients")
+count = 0
+clients = GetRequest("/public/v6/client/clients", "Clients", onepageonly=True)
+DateChange = datetime.datetime.now() - datetime.timedelta(90)
+StartDate = datetime.datetime.strftime(DateChange, '%Y-%m-%dT%H:%M:%S')
+
+visits = {}
+for client in clients:
+    if (client["Status"] == "Active"):
+        result = GetRequest(f'/public/v6/client/clientvisits?clientid={client["UniqueId"]}&StartDate={StartDate}', "Visits")
+        visits[client["UniqueId"]] = len(result)
+
+print(visits)
