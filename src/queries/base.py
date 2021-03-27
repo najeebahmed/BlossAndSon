84ef582e07677ff81d487d814438e7b40ff4f534
Grant <grantddickerson@yahoo.com>
2021-03-26 22:37:56 -0700

    No longer need to use /public/v6/ in GetRequest URI

--- src/queries/base.py
+++ src/queries/base.py
@@ -15,16 +15,30 @@ HEADERS = {
 def GetRequest(uri, array_name, onepageonly=False):
     """The point of this function is..."""
     pages_remaining = 9999
-    if onepageonly:
-        pages_remaining = 1
     results = []
     offset = 0
-    if '?' in uri:
-        bridge = '&'
+    
+    if onepageonly:
+        pages_remaining = 1
+
+    #Backwards compatibility for '/public/v6' prefix and whether intial '/' is used:
+    if uri[0:11] == '/public/v6/':
+        bridge1 = ''
+    elif uri[0:10] == 'public/v6/':
+        bridge1 = '/'
+    elif uri[0] == '/':
+        bridge1 = '/public/v6'
     else:
-        bridge = '?'
+        bridge1 = '/public/v6/'
+    
+    #If '?' Parameter is already used in uri, add '&' to request string, otherwise add the '?'
+    if '?' in uri:
+        bridge2 = '&'
+    else:
+        bridge2 = '?'
+    
     while pages_remaining > 0:
-        conn.request("GET", f'{uri}{bridge}Limit=200&offset={offset}', headers=HEADERS)
+        conn.request("GET", f'{bridge1}{uri}{bridge2}Limit=200&offset={offset}', headers=HEADERS)
         offset += 200
         res = conn.getresponse()
         data = json.loads(res.read().decode('utf-8'))
@@ -40,3 +54,16 @@ def GetRequest(uri, array_name, onepageonly=False):
             pages_remaining = 0
         
     return results
+
+if __name__ == "__main__":
+    clients = GetRequest("/client/clients", "Clients", True)
+    print(clients[15]["FirstName"])
+
+    clients = GetRequest("client/clients", "Clients", True)
+    print(clients[15]["FirstName"])
+
+    clients = GetRequest("/public/v6/client/clients", "Clients", True)
+    print(clients[15]["FirstName"])
+    
+    clients = GetRequest("public/v6/client/clients", "Clients", True)
+    print(clients[15]["FirstName"])
